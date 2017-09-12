f469162ee974eb745ebd3189bad93a05e98ad508
nweiss <neilweiss1@gmail.com>
2017-09-12 14:28:09 -0400

    added working video game

---
+++ Video_Game/Assets/Standard Assets/LSL_BCI_Input.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Threading;
+using LSL;
+using System.Linq;
+
+public class LSL_BCI_Input : MonoBehaviour {
+	public liblsl.StreamOutlet Outlet  = null; //Unity wont recognize Outlet in the Update function unless it is declared globally
+	private liblsl.StreamInlet Inlet = null;
+
+	// Neil 12/07
+	void Start(){
+
+		// Create LSL stream outlet from Unity
+		liblsl.StreamInfo UnityStream = new liblsl.StreamInfo ( "NEDE_Stream", "object_info", 15, 0, liblsl.channel_format_t.cf_float32, "NEDE_position" );
+		Outlet = new liblsl.StreamOutlet(UnityStream);
+		if (Outlet != null){
+			Debug.Log("LSL Stream outlet created");
+		}
+		else{
+			Debug.Log("Error creating LSL stream outlet");
+		}
+
+		// Create LSL stream inlet in Unity
+		Debug.Log("Waiting for classifier stream from Python");
+		liblsl.StreamInfo[] results = liblsl.resolve_stream("name", "Python");
+		Inlet = new liblsl.StreamInlet(results[0]);
+		Debug.Log("Inlet Created: " + Inlet);
+	}
+
+	// pushLSL() function pushes data to the outlet
+	// Outlet is the liblsl.StreamOutlet created above
+	// LSLdata is an array of floats that you want to push
+	public void pushLSL(float[] LSLdata) {
+		Outlet.push_sample(LSLdata);
+	}
+
+	// receiveLSL() function receives data from the Inlet
+	// Inlet is the liblsl.StreamInlet created above
+	// sample is an array of floats that the function returns
+	public float[] receiveLSL(){
+		float[] sample = new float[3];
+		double ts;
+		ts = Inlet.pull_sample(sample, 0.0);
+		return sample;
+	}
+}
