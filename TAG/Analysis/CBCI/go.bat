d1197d4abe2141e808ec4dacea60765b384a1ed7
Jonthan Koss <jdk134@case.edu>
2017-11-09 11:55:12 -0500

    replaced the TAG folder with only the relevant files and functions. Added a TSP folder for the traveling salesman solution. Started to write runTag.m but its still not complete.

--- TAG/Analysis/CBCI/go.bat
+++
@@ -1,4 +0,0 @@
-set EEGTAGPREFIX=\users\mjaswa\Projects\EEG-TAG
-set PATH=.\core;%EEGTAGPREFIX%\ThirdParty\ABM\EEG\SDK\bin;%EEGTAGPREFIX%\ThirdParty\BioSemi\bin\x86;%PATH%
-ProducerConsumer.exe
-pause
